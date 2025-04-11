1a1804822199c2cfbca127f2bf6e4099d42d2cad
Xenon <167872462+XenonBlack999@users.noreply.github.com>
2025-04-11 16:08:18 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	cout << "Wellcome Myanmar"<<endl;
+	cout << "I am CPP developmer"<< endl:
+	cout << "Hello \n\n";
+	cout << "Myanmar";
+	return 0;
+}
